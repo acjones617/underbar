11182a29525f42b6e607a4778498ed86e820e1c2
Andrew Jones <acjones617@gmail.com>
2014-02-24 18:45:03 -0800

    _.uniq completed

--- src/underbar.js
+++ src/underbar.js
@@ -92,6 +92,15 @@ var _ = { };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var unique = [];
+
+    _.each(array, function (element) {
+      if (_.indexOf(unique, element) === -1) {
+        unique.push(element);
+      }
+    });
+
+    return unique;
   };
 
 
